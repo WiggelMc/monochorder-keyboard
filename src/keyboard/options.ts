929d5cb4f1347100cc9c5132ce66b2793482d129
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-04-04 23:43:41 +0200

    Add Options

---
+++ src/keyboard/options.ts
@@ -0,0 +1,77 @@
+import { Vector3 } from "../math/vector3.js"
+
+export interface ShellOptions {
+    thickness: number
+    margin: number
+    curvePointsPerMM: number
+}
+
+export interface FingerDefaultProperties {
+    bevel: number
+    depthOffset: number
+    switchRotation: number
+}
+
+export interface ElementPos {
+    neutralPos: Vector3
+    pressedPos: Vector3
+    lowerPos: Vector3
+}
+
+export type FingerProperties = Partial<FingerDefaultProperties> & ElementPos
+
+export interface FingerOptions {
+    defaults: FingerDefaultProperties
+    pinky: FingerProperties
+    ringFinger: FingerProperties
+    middleFinger: FingerProperties
+    indexFinger: FingerProperties
+    thumb: FingerProperties
+    resetButton: FingerProperties
+}
+
+export interface SocketDefaultProperties {
+    thickness: number
+    holeDiameter: number
+    outerDiameter: number
+}
+
+export type SocketProperties = Partial<SocketDefaultProperties> & ElementPos
+
+export interface SocketOptions {
+    defaults: SocketDefaultProperties
+    topSocket: SocketProperties
+    bottomSocket: SocketProperties
+}
+
+export interface ControllerOptions {
+    thickness: number
+    width: number
+    length: number
+    backSlotWidth: number
+    backSlotDepth: number
+    portOffset: number
+    portDepth: number
+    surfaceOffset: number
+}
+
+export interface RJ9Options {
+    holeHeight: number
+    holeWidth: number
+    slotDepth: number
+    slotDepthOffset: number
+    slotTopOffset: number
+    slotBottomOffset: number
+}
+
+export type PlateOptions = ElementPos & {
+    controller: ControllerOptions
+    rj9: RJ9Options
+}
+
+export interface Options {
+    shell: ShellOptions
+    finger: FingerOptions
+    socket: SocketOptions
+    plate: PlateOptions
+}
