b3fe070ad6b91303719d3d1b5bc77779075f9ca2
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-04-08 00:18:22 +0200

    Add Calibration Pipeline

--- capture/src/main.py
+++ capture/src/main.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Any, Sequence, cast
 import cv2
 from cv2.typing import MatLike, Rect
+import numpy as np
 from pygrabber.dshow_graph import FilterGraph
 import tkinter as tk
 
@@ -61,15 +62,51 @@ if __name__ == "__main__":
 
 
 def stuff():
+    chessPatternSize = [6, 9]
+    calibrationObjectPoints = [[row, column] for row in range(chessPatternSize[0]) for column in range(chessPatternSize[1])]
 
-    imagePoints1 = []
-    imagePoints2 = []
     imageSize = [100, 100]
+
+    cameraMatrix1 = np.array([
+        [imageSize[0], 0, imageSize[0] / 2],
+        [0, imageSize[1], imageSize[1] / 2],
+        [0, 0, 1]
+    ], dtype=np.float64)
+    distCoeffs1 = np.zeros(5, dtype=np.float64)
+    cameraMatrix2 = cameraMatrix1.copy()
+    distCoeffs2 = np.zeros(5, dtype=np.float64)
+
+    calibrationImage1 = [] #Input
+    calibrationImage2 = [] #Input
+
+    retval, corners1 = cast(
+        typ=tuple[bool, MatLike],
+        val=cv2.findChessboardCorners(calibrationImage1, chessPatternSize)
+    )
+
+    retval, corners2 = cast(
+        typ=tuple[bool, MatLike],
+        val=cv2.findChessboardCorners(calibrationImage2, chessPatternSize)
+    )
+
+    retval, cameraMatrix1, distCoeffs1, rvecs1, tvecs1 = cast(
+        typ=tuple[float, MatLike, MatLike, Sequence[MatLike], Sequence[MatLike]],
+        val=cv2.calibrateCamera(calibrationObjectPoints, corners1, imageSize, cameraMatrix1, distCoeffs1)
+    )
+
+    retval, cameraMatrix2, distCoeffs2, rvecs2, tvecs2 = cast(
+        typ=tuple[float, MatLike, MatLike, Sequence[MatLike], Sequence[MatLike]],
+        val=cv2.calibrateCamera(calibrationObjectPoints, corners2, imageSize, cameraMatrix2, distCoeffs2)
+    )
 
     retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cast(
         typ=tuple[float, MatLike, MatLike, MatLike, MatLike, MatLike, MatLike, MatLike, MatLike],
-        val=cv2.stereoCalibrate([], [], [], [], [], [], [], imageSize)
+        val=cv2.stereoCalibrate(calibrationObjectPoints, corners1, corners2, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, imageSize)
     )
+
+
+    imagePoints1 = [] #Input
+    imagePoints2 = [] #Input
 
     R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cast(
         typ=tuple[MatLike, MatLike, MatLike, MatLike, MatLike, Rect, Rect],
@@ -82,8 +119,6 @@ def stuff():
     )
 
 
-    cv2.stereoRectify() # Get Projection Matrices
-    cv2.triangulatePoints() # projection matrices + 2d points => 
 # Steps:
 # - Select Cams
 # - Select Hand
