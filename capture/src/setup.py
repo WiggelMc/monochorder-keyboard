9d3c03c5a176b9b33259b4093f3026a1c9c74b13
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-04-09 01:30:05 +0200

    Restructure setup window

--- capture/src/setup.py
+++ capture/src/setup.py
@@ -4,39 +4,62 @@ import tkinter as tk
 def only_int(input: str):
     return input.isdigit()
 
+def only_filename(input: str):
+    return input.isalpha() or input.isnumeric() or input in ["-", "_"]
+
 def only_float(input: str):
     return input.isdigit() or input == "."
 
 def main():
     root = tk.Tk()
     root.title("Monochorder Capture Setup")
+    root.geometry("400x300")
 
     vcmd_int = (root.register(only_int), '%S')
     vcmd_float = (root.register(only_float), '%S')
+    vcmd_filename = (root.register(only_filename), '%S')
 
-    rows_var = tk.StringVar()
-    columns_var = tk.StringVar()
-    square_width_var = tk.StringVar()
+    def clear():
+        for widget in root.winfo_children():
+            widget.destroy()
 
-    tk.Label(root, text="Rows").grid(row=0, column=0)
-    rows_input = tk.Entry(root, validate="key", validatecommand=vcmd_int, textvariable=rows_var)
-    rows_input.grid(row=0, column=1)
+    def render_calibration():
+        clear()
 
-    tk.Label(root, text="Columns").grid(row=1, column=0)
-    columns_input = tk.Entry(root, validate="key", validatecommand=vcmd_int, textvariable=columns_var)
-    columns_input.grid(row=1, column=1)
+        rows_var = tk.StringVar()
+        tk.Label(root, text="Rows").grid(row=0, column=0)
+        tk.Entry(root, validate="key", validatecommand=vcmd_int, textvariable=rows_var).grid(row=0, column=1, pady=5)
 
-    tk.Label(root, text="Square Width").grid(row=2, column=0)
-    square_width_input = tk.Entry(root, validate="key", validatecommand=vcmd_float, textvariable=square_width_var)
-    square_width_input.grid(row=2, column=1)
+        columns_var = tk.StringVar()
+        tk.Label(root, text="Columns").grid(row=1, column=0)
+        tk.Entry(root, validate="key", validatecommand=vcmd_int, textvariable=columns_var).grid(row=1, column=1, pady=5)
 
-    def submit():
-        print(int(rows_var.get()))
-        print(int(columns_var.get()))
-        print(float(square_width_var.get()))
+        square_width_var = tk.StringVar()
+        tk.Label(root, text="Square Width").grid(row=2, column=0)
+        tk.Entry(root, validate="key", validatecommand=vcmd_float, textvariable=square_width_var).grid(row=2, column=1, pady=5)
 
-    calibrate_button = tk.Button(text="Calibrate [C]", command=submit)
-    calibrate_button.grid(row=4, column=0, columnspan=2)
+        def submit():
+            print(int(rows_var.get()))
+            print(int(columns_var.get()))
+            print(float(square_width_var.get()))
+            render_image_creation()
+
+        tk.Button(root, text="Calibrate [C]", command=submit).grid(row=3, column=0, columnspan=2, pady=20)
+
+    def render_image_creation():
+        clear()
+
+        name_var = tk.StringVar()
+        tk.Label(root, text="Rows").grid(row=0, column=0)
+        tk.Entry(root, validate="key", validatecommand=vcmd_filename, textvariable=name_var).grid(row=0, column=1, pady=5)
+
+        def submit():
+            print(name_var.get())
+            root.quit()
+
+        tk.Button(root, text="Save and Quit [C]", command=submit).grid(row=1, column=0, columnspan=2, pady=20)
+
+    render_calibration()
     root.mainloop()
 
 if __name__ == "__main__":
